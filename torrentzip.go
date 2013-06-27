227b0eaaed46b5bced0e81726ce2015bf1c3e38c
Uwe Hoffmann <uwe@codemanic.com>
2013-06-27 16:12:21 -0700

    added back in zip64 support

--- torrentzip.go
+++ torrentzip.go
@@ -56,6 +56,19 @@ const (
 	creatorFAT = 0
 
 	zipVersion20 = 20 // 2.0
+	zipVersion45 = 45 // 4.5 (reads and writes zip64 archives)
+
+	directory64LocSignature = 0x07064b50
+	directory64EndSignature = 0x06064b50
+	directory64LocLen       = 20 //
+	directory64EndLen       = 56 // + extra
+
+	// limits for non zip64 files
+	uint16max = (1 << 16) - 1
+	uint32max = (1 << 32) - 1
+
+	// extra header id's
+	zip64ExtraId = 0x0001 // zip64 Extended Information Extra Field
 )
 
 type Writer struct {
@@ -146,7 +159,7 @@ func (w *Writer) Close() error {
 	for _, fi := range fis {
 		fh := r.File[fi.index]
 		fi.offset = cw.count
-		err = writeHeader(cw, fh, fi.name)
+		err = writeHeader(cw, fh, fi.name, fi.offset)
 		if err != nil {
 			return err
 		}
@@ -174,14 +187,51 @@ func (w *Writer) Close() error {
 	}
 	end := cw.count
 
+	records := uint64(len(fis))
+	size := uint64(end - start)
+	offset := uint64(start)
+
+	if records > uint16max || size > uint32max || offset > uint32max {
+		var buf [directory64EndLen + directory64LocLen]byte
+		b := writeBuf(buf[:])
+
+		// zip64 end of central directory record
+		b.uint32(directory64EndSignature)
+		b.uint64(directory64EndLen)
+		b.uint16(zipVersion45) // version made by
+		b.uint16(zipVersion45) // version needed to extract
+		b.uint32(0)            // number of this disk
+		b.uint32(0)            // number of the disk with the start of the central directory
+		b.uint64(records)      // total number of entries in the central directory on this disk
+		b.uint64(records)      // total number of entries in the central directory
+		b.uint64(size)         // size of the central directory
+		b.uint64(offset)       // offset of start of central directory with respect to the starting disk number
+
+		// zip64 end of central directory locator
+		b.uint32(directory64LocSignature)
+		b.uint32(0)           // number of the disk with the start of the zip64 end of central directory
+		b.uint64(uint64(end)) // relative offset of the zip64 end of central directory record
+		b.uint32(1)           // total number of disks
+
+		if _, err := cw.Write(buf[:]); err != nil {
+			return err
+		}
+
+		// store max values in the regular end record to signal that
+		// that the zip64 values should be used instead
+		records = uint16max
+		size = uint32max
+		offset = uint32max
+	}
+
 	var buf [directoryEndLen]byte
 	b := writeBuf(buf[:])
 	b.uint32(uint32(directoryEndSignature))
 	b.uint16(0)
 	b.uint16(0)
-	b.uint16(uint16(len(fis)))
-	b.uint16(uint16(len(fis)))
-	b.uint32(uint32(end - start))
+	b.uint16(uint16(records))
+	b.uint16(uint16(records))
+	b.uint32(uint32(size))
 	b.uint32(uint32(start))
 	b.uint16(22)
 
@@ -205,27 +255,50 @@ func (w *Writer) Close() error {
 	return nil
 }
 
-func writeHeader(w io.Writer, h *czip.File, canonicalName string) error {
+func writeHeader(w io.Writer, h *czip.File, canonicalName string, offset int64) error {
 	var buf [fileHeaderLen]byte
 	b := writeBuf(buf[:])
 	b.uint32(uint32(fileHeaderSignature))
-	b.uint16(zipVersion20)
+	if isZip64(h) {
+		b.uint16(zipVersion45)
+	} else {
+		b.uint16(zipVersion20)
+	}
 	b.uint16(2)
 	b.uint16(8)
 	b.uint16(48128)
 	b.uint16(8600)
 	b.uint32(h.CRC32)
-	b.uint32(h.CompressedSize)
-	b.uint32(h.UncompressedSize)
+	if isZip64(h) {
+		// the file needs a zip64 header. store maxint in both
+		// 32 bit size fields (and offset later) to signal that the
+		// zip64 extra header should be used.
+		b.uint32(uint32max) // compressed size
+		b.uint32(uint32max) // uncompressed size
+
+		// append a zip64 extra block to Extra
+		var buf [28]byte // 2x uint16 + 3x uint64
+		eb := writeBuf(buf[:])
+		eb.uint16(zip64ExtraId)
+		eb.uint16(24) // size = 3x uint64
+		eb.uint64(h.UncompressedSize64)
+		eb.uint64(h.CompressedSize64)
+		eb.uint64(uint64(offset))
+		h.Extra = append(h.Extra, buf[:]...)
+	} else {
+		b.uint32(h.CompressedSize)
+		b.uint32(h.UncompressedSize)
+	}
 	b.uint16(uint16(len(canonicalName)))
-	b.uint16(0)
+	b.uint16(uint16(len(h.Extra)))
 	if _, err := w.Write(buf[:]); err != nil {
 		return err
 	}
 	if _, err := io.WriteString(w, canonicalName); err != nil {
 		return err
 	}
-	return nil
+	_, err := w.Write(h.Extra)
+	return err
 }
 
 func writeCentralHeader(w io.Writer, h *czip.File, canonicalName string, offset int64) error {
@@ -233,28 +306,60 @@ func writeCentralHeader(w io.Writer, h *czip.File, canonicalName string, offset 
 	b := writeBuf(buf[:])
 	b.uint32(uint32(directoryHeaderSignature))
 	b.uint16(creatorFAT)
-	b.uint16(zipVersion20)
+	if isZip64(h) {
+		b.uint16(zipVersion45)
+	} else {
+		b.uint16(zipVersion20)
+	}
 	b.uint16(2)
 	b.uint16(8)
 	b.uint16(48128)
 	b.uint16(8600)
 	b.uint32(h.CRC32)
-	b.uint32(h.CompressedSize)
-	b.uint32(h.UncompressedSize)
+	if isZip64(h) || offset > uint32max {
+		// the file needs a zip64 header. store maxint in both
+		// 32 bit size fields (and offset later) to signal that the
+		// zip64 extra header should be used.
+		b.uint32(uint32max) // compressed size
+		b.uint32(uint32max) // uncompressed size
+
+		// append a zip64 extra block to Extra
+		var buf [28]byte // 2x uint16 + 3x uint64
+		eb := writeBuf(buf[:])
+		eb.uint16(zip64ExtraId)
+		eb.uint16(24) // size = 3x uint64
+		eb.uint64(h.UncompressedSize64)
+		eb.uint64(h.CompressedSize64)
+		eb.uint64(uint64(offset))
+		h.Extra = append(h.Extra, buf[:]...)
+	} else {
+		b.uint32(h.CompressedSize)
+		b.uint32(h.UncompressedSize)
+	}
 	b.uint16(uint16(len(canonicalName)))
-	b.uint16(0)
+	b.uint16(uint16(len(h.Extra)))
 	b.uint16(0)
 	b.uint16(0)
 	b.uint16(0)
 	b.uint32(0)
-	b.uint32(uint32(offset))
+	if offset > uint32max {
+		b.uint32(uint32max)
+	} else {
+		b.uint32(uint32(offset))
+	}
 	if _, err := w.Write(buf[:]); err != nil {
 		return err
 	}
 	if _, err := io.WriteString(w, canonicalName); err != nil {
 		return err
 	}
-	return nil
+
+	_, err := w.Write(h.Extra)
+	return err
+}
+
+func isZip64(h *czip.File) bool {
+	return h.CompressedSize64 > uint32max || h.UncompressedSize64 > uint32max
 }
 
 func (w *Writer) Create(name string) (io.Writer, error) {
